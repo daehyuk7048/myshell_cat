dc1b2b85b162968150ff6e24f37fa98c5cec0acb
Kang,Woochul <sindaehyuk@gmail.com>
2024-12-05 17:15:39 +0900

    successful cat

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf(" hellow shell world\n");
+    return 0;
+}
--- src/myshell.c
+++ src/myshell.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "ls_command.h"
 #define MAX_LINE 80
 #define MAX_ARGS 10
 
@@ -17,11 +21,11 @@ int main(){
         fgets(input,MAX_LINE,stdin);
         printf("%s\n",input);
 
-        token=strtok(input, "\n\t");
+        token=strtok(input, " \n\t");
         int i=0;
         while(token!=NULL){
             argv[i++]=token;
-            token=strtok(NULL, "\n\t");
+            token=strtok(NULL, " \n\t");
         }
         argv[i]=NULL;
 
@@ -37,6 +41,43 @@ int main(){
         } else if(strcmp(argv[0],"pwd")==0){
             getcwd(input,MAX_LINE);
             printf("%s\n",input);
+        } else if(strcmp(argv[0],"ls")==0){
+            my_ls();
+        } else if(strcmp(argv[0],"cat")==0){
+            for (int j = 1; argv[j] != NULL; j++) {
+                    int fd = open(argv[j], O_RDONLY); 
+                    if (fd == -1) {
+                        perror("cat");
+                        continue;
+                    }
+
+                    char buffer[MAX_LINE];
+                    ssize_t bytes_read;
+                    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+                        buffer[bytes_read] = '\0'; 
+                        write(STDOUT_FILENO, buffer, bytes_read);
+                    }
+                    printf("\n");
+                    if (bytes_read == -1) {
+                        perror("read");
+                    }
+
+                    close(fd);
+                }
+             
+        } else{
+            pid_t pid =fork();
+           if (pid == 0) {
+                if (access(argv[0], X_OK) == 0) {
+                    execv(argv[0], argv); 
+                    perror("execv"); 
+                } else {
+                    printf("command not found: %s\n", argv[0]);
+                }
+                exit(EXIT_FAILURE);
+            } else {
+                wait(NULL);
+            }
         }
     }
     
